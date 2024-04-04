0dbd1172e1863a3ad8d2d8d78578f470979ca1b9
Federico Miori <federicomimart@gmail.com>
2024-04-04 13:16:41 +0200

    Mejorado comentarios y correccion de bugs menores

--- src/Tablero.java
+++ src/Tablero.java
@@ -30,7 +30,10 @@ public class Tablero {
     }
 
     public Pieza getPiezaEnCasilla(Posicion posicion){
-        return getCasilla(posicion).getPieza();
+        if(posicion.dentroLimites(ALTO_TABLERO,ANCHO_TABLERO))
+            return getCasilla(posicion).getPieza();
+        else
+            return null;
     }
 
     /**
@@ -108,35 +111,37 @@ public class Tablero {
 
     /**
      * Metodo que mueve la pieza situalda en posicionOrigen a la posicionDestino
+     * Tiene en cuenta toda la casuistica para ver si se puede realizar ese movimiento
      */
     public void moverPieza(Posicion posicionOrigen, Posicion posicionDestino){
         boolean autorizadoAMover = false;
         Pieza piezaAMover = getPiezaEnCasilla(posicionOrigen);
         Pieza piezaEnDestino = getPiezaEnCasilla(posicionDestino);
         int[] vectorMovimiento = posicionOrigen.getVector( posicionDestino );
-        //primero comprobamos que ambas posiciones esten dentro del tablero para evitar errores
+
+        //Comprueba que las posiciones estén dentro del tablero
         if(posicionOrigen.dentroLimites(ALTO_TABLERO, ANCHO_TABLERO) && posicionDestino.dentroLimites(ALTO_TABLERO, ANCHO_TABLERO)){
-            //Segundo comprueba que en la posicion selecionada haya una fichaa que mover
-            //y se comprueba que el movimiento sea valido para esa pieza pasandole el vector del movimiento
-            //Ej: una torre no acepta un movimiento diagonal
-            if(piezaAMover != null && piezaAMover.movimientoValido(vectorMovimiento))
-                //Tercero se comprueba que en la trayectoria entre orijen y destino no haya otras piezas
-                //o en su defecto que la ficha a mover sea un caballo ya que puede saltar por encima de otras piezas
+            //Consulta a la pieza si puede hacer ese movimiento (Aprovechando el polimorfismo)
+            if(piezaAMover != null && piezaAMover.movimientoValido(vectorMovimiento)) {
+                //Comprueba que no haya ninguna ficha entre las 2 posiciones excepto que sea un caballo
                 if (caminoDespejado(posicionOrigen, posicionDestino) || piezaAMover instanceof Caballo)
-                    //y por ultimo que la casilla destino este vacia o que la pieza sea del coloe opuesto
-                    // ya que no se puede mover una pieza a una casilla donde hay otra pieza del mismo color
+                    //Comprueba que la casilla destino esté vacia o que contenga una ficha adversaria
                     if (piezaEnDestino == null || piezaEnDestino.distintoColor(piezaAMover))
                         autorizadoAMover = true;
-        }
-        else if(piezaAMover instanceof Peon && piezaAMover.movimientoValido(vectorMovimiento, piezaEnDestino))
-            autorizadoAMover = true;
+            }
 
+            //Aquí se tiene en cuenta la situacion particular en la que el peon puede comer moviendose en diagonal
+            else if(piezaAMover instanceof Peon && piezaAMover.movimientoValido(vectorMovimiento, piezaEnDestino))
+                autorizadoAMover = true;
+        }
+
+        //Aquí hace el movimiento como tal
         if(autorizadoAMover){
             piezaAMover.incrementarNumMovimientos();
             getCasilla(posicionOrigen).setPieza(null);
             getCasilla(posicionDestino).setPieza(piezaAMover);
         }else
-            System.out.println("No se pudo realizar el movimiento");
+            System.out.println("\nERROR: No se pudo realizar el movimiento");
     }
 
     /**
@@ -148,23 +153,37 @@ public class Tablero {
      */
     public boolean caminoDespejado(Posicion origen, Posicion destino){
         int[] vector = origen.getVector(destino);
+        int[] vectorUnitario = getVectorUnitario(vector);
+
         boolean sinObstaculos = true;
         Posicion posicionIteradora = new Posicion( origen.getPosX(), origen.getPosY() );
+
+        //Aquí se comprueba que el vector sea diagonal, horizontal o diagonal
         if( vector[0]*vector[1] == 0 || Math.abs(vector[0]) == Math.abs(vector[1]) ){
-            for (int i = 0; i < vector.length; i++) {
-                if(vector[i] < 0) vector[i] = -1;
-                else if(vector[i] > 0) vector[i] = 1;
-            }
-            posicionIteradora.sumarVector( vector );
-            while(!posicionIteradora.equals(destino)
+            //a la posicion iteradora le sumo el vector unitario
+            posicionIteradora.sumarVector( vectorUnitario );
+            while( ! posicionIteradora.equals(destino)
                     && posicionIteradora.dentroLimites(ANCHO_TABLERO,ALTO_TABLERO)
                     && sinObstaculos){
                 if(!casillaVacia(posicionIteradora)) {
                     sinObstaculos = false;
                 }
-                posicionIteradora.sumarVector(vector);
+                posicionIteradora.sumarVector(vectorUnitario);
             }
         }
         return sinObstaculos;
+    }
+
+    private static int[] getVectorUnitario(int[] vector){
+        int[] vectorUnitario= new int[vector.length];
+        for (int i = 0; i < vector.length; i++) {
+            if(vector[i] < 0)
+                vectorUnitario[i] = -1;
+            else if(vector[i] > 0)
+                vectorUnitario[i] = 1;
+            else
+                vectorUnitario[i] = 0;
+        }
+        return vectorUnitario;
     }
 }
--- src/tiposPieza/Pieza.java
+++ src/tiposPieza/Pieza.java
@@ -1,6 +1,6 @@
 package tiposPieza;
 public abstract class Pieza {
-    private char letra;
+    private final char letra;
     protected Color color;
 
     final String parametroVerde = "\u001B[38;2;55;139;71m";
@@ -23,30 +23,35 @@ public abstract class Pieza {
         return numMovimientos;
     }
 
-    public String toString(){
-        if(esDeColor(Color.negro))
-            return parametroVerde + letra + resetTipografia;
-        else
-            return String.valueOf(letra);
-    }
-
-    public void incrementarNumMovimientos(){
-        numMovimientos++;
-    }
-
     //Defino este metodo abstracto para obligar a quee todos los hijos implementen este metodo
     public abstract boolean movimientoValido(int movimientoHorizontal, int movimientoVertical);
 
+    /**
+     * Sobre carga del metodo movimiento valido para poder haceptar un vector en un array
+     * y hacer así un codigo mas limpio
+     */
     public boolean movimientoValido(int[] vectorMovimiento){
         return movimientoValido(vectorMovimiento[0], vectorMovimiento[1]);
     }
 
+    /**
+     * Este metod valida el movimiento para un caso en el que el destino contiene una pieza distinta de nulo
+     * (es decir donde la casilla no esta vacia)
+     * Esto sirve para casos concretos como cuando un peon solo puede moverse en diagonal Si y solo si puede comer una pieza
+     * @param vectorMovimiento aui va la vectorizacion de su movimiento
+     * @param piezaAComer y aquí va la pieza de la casilla destino para ver si es del color opuesto
+     * @return Devuleve true en caso de validar el movimiento
+     */
     public boolean movimientoValido(int[] vectorMovimiento, Pieza piezaAComer){
         if(this instanceof Peon)
             //Esta linea ejecuta el metodo de la clase hija peon
-            return ((Peon) this).movimientoValido(vectorMovimiento, piezaAComer);
+            return ((Peon) this).peonPuedeComer(vectorMovimiento, piezaAComer);
         else
             return this.movimientoValido(vectorMovimiento);
+    }
+
+    public void incrementarNumMovimientos(){
+        numMovimientos++;
     }
 
     public boolean esDeColor(Color color){
@@ -60,4 +65,12 @@ public abstract class Pieza {
     public boolean distintoColor(Pieza piezaEntrada){
         return piezaEntrada == null || piezaEntrada.getColor() != color;
     }
+
+    public String toString(){
+        if(esDeColor(Color.negro))
+            return parametroVerde + letra + resetTipografia;
+        else
+            return String.valueOf(letra);
+    }
+
 }
